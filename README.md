561cdc9256940cb36499d0ec2e8a89f0004d40c5
Tofunmi1 <67348569+Tofunmi1@users.noreply.github.com>
2021-12-25 01:19:31 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# encryption_with_js
-short code and challenge to test encryption in javascript
+##Encryption Testing with javascript
+
+##remove or delete package-lock.json and run 
+
+
+``npm i && npm start``
+
+##I am using babel, because you cant use import statements in pure node
+
+####<web3bridge/>
+
+
+
--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-import { CryptoJS } from 'aes-js'
+import SimpleCrypto from "simple-crypto-js"
 
-const encryptData = (message, key) => {
-  const encryptedData = CryptoJS.AES.encrypted(message, key)
-  return encryptData.toString()
+const encryptMessage = ({string, our_key=''}) => {
+  const secretKey = our_key
+  const simpleCrypto = new SimpleCrypto(secretKey)
+  const cipherText = simpleCrypto.encrypt(string)
+  return cipherText
 }
 
-const decryptData = (encryptedData, key) => {
-  const decryptedData = CryptoJS.AES.decrypt(encryptData, key)
-  return decryptedData.toString(CryptoJS.enc.Utf8)
+const decryptMessage = ({encrypted_message, our_key=''}) => {
+  const simpleCrypto = new SimpleCrypto(our_key)
+  return simpleCrypto.decrypt(encrypted_message)
 }
 
-console.log(encryptData('Hello World', 'key'))
-console.log(decryptData('U2FsdGVkX1/0oPpnJ5S5XTELUonupdtYCdO91v+/SMs=', 'key'))
-
+console.log(encryptMessage('Hello World','key'))
+console.log(decryptMessage('b8eb6d3daf6a8afcef2dda74dcf0df8bd30e7d6cdec60a211a31a483d110db6cSFrCvKlgHG5aD/IMmtBThg==9de64a116cd01c0bd3895121a5a74d63ec72e2bbe4ff29a60e80d2220a53f8f6', 'our_key'))
